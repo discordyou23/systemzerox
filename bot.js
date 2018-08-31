e200e62311356afa73f72a3192a06c092571d8b8
Fofodiscord <42048326+discordyou23@users.noreply.github.com>
2018-08-31 15:02:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,4 +83,25 @@ msg.delete();
 
 
 
+client.on('message',async Epic => {
+  var codes = "#";
+  if(Epic.content.startsWith(codes + "onlinezerox")) {
+  if(!Epic.guild.member(Epic.author).hasPermissions('MANAGE_CHANNELS')) return Epic.reply(':x: **ليس لديك الصلاحيات الكافية**');
+  if(!Epic.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return Epic.reply(':x: **ليس معي الصلاحيات الكافية**');
+  Epic.guild.createChannel(`Voice Online : [ ${Epic.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice Online Is Activation In ${Epic.guild.name}`);
+    c.overwritePermissions(Epic.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(` Online :  ${Epic.guild.members.filter(m => m.voiceChannel).size} .`)
+    },1000);
+  });
+  }
+});
+
+
+
+
 client.login(process.env.BOT_TOKEN);
